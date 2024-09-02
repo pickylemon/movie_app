471b7e3dc673bd41e2d5c28a4534cdcf6cc6cebb
pickylemon <novecento104@gmail.com>
2024-09-02 19:52:08 +0900

    실시간 이미지 리사이징 및 Search bar에 SearchText 유지시키기

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,10 @@ export default class Search extends Component {
     render() {
         this.el.classList.add('search')
         this.el.innerHTML = /* html */`
-            <input placeholder="Enter the movie title to search!" />
+            <input 
+                placeholder="Enter the movie title to search!" 
+                value="${movieStore.state.searchText}"/>
+            <!-- 검색 내용이 그대로 검색 필드에 남는다. 상세 페이지에서 뒤로가기 해도 그대로 유지됨 -->
             <button class="btn btn-primary">
                 Search!
             </button>
--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -7,11 +7,14 @@ export default class Movie extends Component {
         await getMovieDetails(history.state.id)
         console.log(movieStore.state.movie)
         const { movie } = movieStore.state
+        const bigPoster = movie.Poster.replace('SX300', 'SX700') 
+        //실시간 리사이징(서버로부터 이미지 사이즈를 실시간으로 리사이징해서 받아옴)
+
 
         this.el.classList.add('container', 'the-movie')
         this.el.innerHTML = /* html */`
             <div 
-                style="background-image: url(${movie.Poster})"
+                style="background-image: url(${bigPoster})"
                 class="poster"></div>
             <div class="specs">
                 <div class="title">
